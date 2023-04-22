c01e541c4257b9927dcd79d98b6fa69d281ef49c
essex999 <oktav2222@gmail.com>
2023-04-22 22:55:35 +0300

    first commit

---
+++ templates.js
@@ -0,0 +1,24 @@
+function gameStart() {
+    return {
+        tag:'div', class:'game',content:[
+            {tag:'div',cls:'section',content:[
+                {tag:'div',cls:'section_content',content:[
+                    {tag:'p',cls:'section-title',content:'Выбери сложность'},
+                    {tag:'div',cls:'section_selectors',content:[
+                        {tag:'div',cls:'section_selector',content:'1'},
+                        {tag:'div',cls:'section_selector',content:'2'},
+                        {tag:'div',cls:'section_selector',content:'3'},
+                    ]},
+                {tag:'button',cls:'sectionButton',content:'Старт'},
+                ]}
+            
+            ]}
+        ]
+    }
+};
+
+
+
+
+
+
